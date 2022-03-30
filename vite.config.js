9bf908331f35ecf2f3de4af03eef6f6a7b2ec321
szamanr <szamanr@protonmail.com>
2022-03-30 13:48:29 +0200

    configure build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'use-layers',
+      entry: 'src/index.ts',
+      fileName: (format) => `index.${format}.js`,
+      name: 'index.js',
     },
     rollupOptions: {
       // externalize deps that shouldn't be bundled
