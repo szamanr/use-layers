8f8b5397cb5c5a8d18ea6590163f307b47024fb7
szamanr <szamanr@protonmail.com>
2022-04-15 00:02:07 +0200

    use tailwind in examples

--- examples/use-layers/index.tsx
+++ examples/use-layers/index.tsx
@@ -2,6 +2,7 @@ import { UseLayersExample } from "./UseLayersExample";
 import React from "react";
 import { render } from "react-dom";
 import { Layers } from "../../src";
+import "./index.css";
 
 render(
   <Layers options={{ baseIndex: 1000 }}>
