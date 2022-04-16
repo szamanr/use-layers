934cb246a736317d3fff74e624be173ba676902b
szamanr <szamanr@protonmail.com>
2022-04-17 00:07:54 +0200

    🚧 add tests for showing / hiding layers

--- src/Layer.test.tsx
+++ src/Layer.test.tsx
@@ -1,7 +1,35 @@
 import { cleanup, render, screen } from "@testing-library/react";
-import React from "react";
+import React, { useState } from "react";
 import { afterEach, expect, it } from "vitest";
 import { Layer, Layers } from "./index";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+const TestComponent = () => {
+  const [showModal0, setShowModal0] = useState(false);
+  const [showModal1, setShowModal1] = useState(false);
+
+  return (
+    <Layers>
+      <button onClick={() => setShowModal0((s) => !s)}>toggle modal 0</button>
+      {showModal0 && (
+        <Layer>
+          <article title="evil courage">
+            The evil courage of samadhi is to avoid with dimension.
+          </article>
+        </Layer>
+      )}
+      <button onClick={() => setShowModal1((s) => !s)}>toggle modal 1</button>
+      {showModal1 && (
+        <Layer>
+          <article title="love sex">
+            Love is the only sex, the only guarantee of peace.
+          </article>
+        </Layer>
+      )}
+    </Layers>
+  );
+};
 
 afterEach(() => {
   cleanup();
@@ -50,4 +78,47 @@ it("should use custom step", () => {
 
   const element = screen.getByRole("article", { name: /evil courage/i });
   expect(element.dataset.zIndex).toEqual("40");
+});
+
+it("should set the zIndex in the order of render", () => {
+  render(
+    <Layers>
+      <Layer>
+        <article title="evil courage">
+          The evil courage of samadhi is to avoid with dimension.
+        </article>
+      </Layer>
+      <Layer>
+        <article title="love sex">
+          Love is the only sex, the only guarantee of peace.
+        </article>
+      </Layer>
+    </Layers>
+  );
+
+  const elements = [
+    screen.getByRole("article", { name: /evil courage/i }),
+    screen.getByRole("article", { name: /love sex/i }),
+  ];
+  expect(elements[0].dataset.zIndex).toEqual("10");
+  expect(elements[1].dataset.zIndex).toEqual("20");
+});
+
+it("should unset the zIndex when element removed from DOM", () => {
+  render(<TestComponent />);
+
+  const toggleModal0 = screen.getByRole("button", { name: /toggle modal 0/ });
+  const toggleModal1 = screen.getByRole("button", { name: /toggle modal 1/ });
+
+  const elements: [string, { name: RegExp }][] = [
+    ["article", { name: /evil courage/i }],
+    ["article", { name: /love sex/i }],
+  ];
+
+  expect(screen.queryByRole(...elements[0])).toBeUndefined();
+  expect(screen.queryByRole(...elements[1])).toBeUndefined();
+
+  userEvent.click(toggleModal0);
+
+  expect(screen.getByRole(...elements[0]).dataset.zIndex).toEqual("10");
 });
--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,8 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: false,
     environment: "jsdom",
